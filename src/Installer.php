786e295e9ed4ab33a243daacf6126f495305925c
Amiel Elboim <amielel@matrix.co.il>
2017-12-13 21:27:22 +0200

    add file to gitignore

--- src/Installer.php
+++ src/Installer.php
@@ -16,18 +16,43 @@ use Composer\Repository\InstalledRepositoryInterface ;
 
 /**
  * Class Installer
- * This class extends a functionality of the install and update command of composer
+ * This class extends a functionality of the install and update commands of composer
  * @package Clinikal\ComposerInstallersClinikalExtender
  */
 class Installer extends ExtenderInstaller
 {
+    const VERTICAL_ADDONS = 'clinikal-vertical';
+    const ZF_MODULES = 'openemr-zf-modules';
+    const FORMHANDLER_FORMS = 'openemr-formhandler-forms';
 
+    private $basePath;
+
+
+    /**
+     * init private properties and object for this class
+     */
+    private function initClinikal()
+    {
+        $this->basePath = dirname($this->vendorDir) .'/';
+    }
+    
     /**
      * {@inheritDoc}
      */
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
+    {   
+        $this->initClinikal();
+        //composer install
         LibraryInstaller::install($repo,$package);
+        $this->appendToGitignore($this->getInstallPath($package));
+
+        //spacial actions per package type
+        switch ($package->getType())
+        {
+            case self::FORMHANDLER_FORMS:
+
+                break;
+        }
 
     }
 
@@ -37,10 +62,11 @@ class Installer extends ExtenderInstaller
      */
     public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
     {
+        $this->initClinikal();
         LibraryInstaller::update($repo,$initial, $target);
 
         if ($target->getType() === 'openemr-formhandler-forms') {
-            copy($this->vendorDir .'/../' .$this->getInstallPath($target).'/'. explode('/',$target->getName())[1].'.json', $this->vendorDir .'/../clinikal/install/couchDB/forms/backup_data/'. explode('/',$target->getName())[1].'.json');
+            copy($this->basePath .$this->getInstallPath($target).'/'. explode('/',$target->getName())[1].'.json', $this->basePath .'clinikal/install/couchDB/forms/backup_data/'. explode('/',$target->getName())[1].'.json');
         }
 
         /*print_r($target->getName());
@@ -55,6 +81,23 @@ class Installer extends ExtenderInstaller
         print_r($initial->getType());
         print_r($this->getInstallPath($initial));*/
 
+    }
+
+    private function appendToGitignore($ignoreFile)
+    {
+        file_put_contents($this->basePath.'.gitignore', PHP_EOL . $ignoreFile, FILE_APPEND);
+        $this->messageToCLI('Add to .gitignore - ' . $ignoreFile);
+    }
+
+    private function getFolderName($fullName)
+    {
+        list($prefixName, $folderName) = explode('/',$fullName);
+        return !is_null($folderName) ? $folderName : $prefixName;
+    }
+
+    private function messageToCLI($message)
+    {
+        fwrite(STDOUT,$message . PHP_EOL);
     }
 
 
