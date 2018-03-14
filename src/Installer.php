e30ffc13e66943a367abbbfcd20e55b5be2cfeb7
Amiel Elboim <amielel@matrix.co.il>
2018-03-14 18:00:44 +0200

    remove external installer

--- src/Installer.php
+++ src/Installer.php
@@ -10,7 +10,6 @@ namespace Clinikal\ComposerInstallersClinikalExtender;
 
 use Composer\Installer\LibraryInstaller;
 use Composer\Installers\Installer as ComposerInstaller;
-use OomphInc\ComposerInstallersExtender\Installer as ExtenderInstaller;
 use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface ;
 use Clinikal\ComposerInstallersClinikalExtender\FormhandlerActions;
@@ -22,7 +21,7 @@ use Clinikal\ComposerInstallersClinikalExtender\Zf2ModulesActions;
  * This class extends a functionality of the install and update commands of composer
  * @package Clinikal\ComposerInstallersClinikalExtender
  */
-class Installer extends ExtenderInstaller
+class Installer extends ComposerInstaller
 {
     /* custom packages's types */
     const VERTICAL_ADDONS = 'clinikal-vertical-addons';
@@ -40,6 +39,29 @@ class Installer extends ExtenderInstaller
     public $isZero;
     private $isInit = false;
 
+    protected $packageTypes;
+
+    public function getInstallPath( PackageInterface $package ) {
+        $installer = new InstallerHelper( $package, $this->composer, $this->io );
+        $path = $installer->getInstallPath( $package, $package->getType() );
+        // if the path is false, use the default installer path instead
+        return $path !== false ? $path : LibraryInstaller::getInstallPath( $package );
+    }
+
+    public function supports( $packageType ) {
+        // grab the package types once
+        if ( !isset( $this->packageTypes ) ) {
+            $this->packageTypes = false;
+            if ( $this->composer->getPackage() ) {
+                // get data from the 'extra' field
+                $extra = $this->composer->getPackage()->getExtra();
+                if ( !empty( $extra['installer-types'] ) ) {
+                    $this->packageTypes = (array) $extra['installer-types'];
+                }
+            }
+        }
+        return is_array( $this->packageTypes ) && in_array( $packageType, $this->packageTypes );
+    }
 
     /**
      * init private properties and objects for clinikal project
