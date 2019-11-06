b8d1ec922cb3098c44d2acf77a388f97d9325302
Amiel Elboim <amielel@matrix.co.il>
2019-11-06 12:32:48 +0200

    fix

--- src/ReactActions.php
+++ src/ReactActions.php
@@ -15,13 +15,10 @@ class ReactActions
     static function createReactLInk(Installer $installer, PackageInterface $package)
     {
         if (!is_dir($installer->getInstallPath($package).'/' . self::SOURCE_REACT)) return;
-        if (!is_dir($installer->basePath.self:: DEST_REACT)) {
-            mkdir($installer->basePath.self:: DEST_REACT);
-        }
 
-        $baseTarget = Installer::getRelativePathBetween($installer->basePath.self:: DEST_REACT, $installer->basePath);
+       // $baseTarget = Installer::getRelativePathBetween($installer->basePath.self:: DEST_REACT, $installer->basePath);
         if (!is_link($installer->basePath.self::DEST_REACT)) {
-            symlink($baseTarget.$installer->getRelativePath($package).'/'.self::SOURCE_REACT ,$installer->basePath.self::DEST_REACT);
+            symlink('openemr/'.$installer->getRelativePath($package).'/'.self::SOURCE_REACT ,$installer->basePath.self::DEST_REACT);
         }
     }
 }
